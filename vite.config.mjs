94f66df202e13436e4e3c633aa7a31f6b837230d
Aakash Bansal <abansal0486@gmail.com>
2025-08-14 14:28:18 +0530

    Added server config

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,7 +5,7 @@ import path from 'path';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const API_URL = env.VITE_APP_BASE_NAME || '/';
+  const API_URL = env.VITE_APP_BASE_NAME || '/admin/';
   const PORT = 3000;
 
   return {
